bb11f185c383f6cbdcfdaab2fa6e1037b71acd81
Julia Nguyen <juliatn5@uci.edu>
2025-03-15 02:33:35 +0000

    add print_path

---
+++ src/dijkstras.cpp
@@ -0,0 +1,16 @@
+#include "dijkstras.h"
+#include <iostream>
+
+// vector<int> dijkstra_shortest_path(const Graph& G, int source, vector<int>& previous) {
+
+// }
+
+// vector<int> extract_shortest_path(const vector<int>& distances, const vector<int>& previous, int destination) {
+
+// }
+
+void print_path(const vector<int>& v, int total) {
+    for (int i = 0; i < total; ++i) {
+        std::cout << v[i] << " ";
+    }
+}
