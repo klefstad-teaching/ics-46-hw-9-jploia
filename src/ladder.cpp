638fdfae61aab601ca38ea062fbc90e119db839f
Julia Nguyen <juliatn5@uci.edu>
2025-03-16 20:22:36 +0000

    add print_word_ladder

--- src/ladder.cpp
+++ src/ladder.cpp
@@ -17,25 +17,6 @@ bool is_adjacent(const string& word1, const string& word2) {
 }
 
 vector<string> generate_word_ladder(const string& begin_word, const string& end_word, const set<string>& word_list) {
-    // function generate_word_ladder(begin_word, end_word, word_list):
-    //     ladder_queue = queue of vector of strings
-    //     ladder_queue.push([begin_word])
-    //     visited = set of strings
-    //     visited.insert(begin_word)
-    //     while ladder_queue is not empty:
-    //         ladder = ladder_queue.pop_front()
-    //         last_word = last element of ladder
-    //         for word in word_list:
-    //             if is_adjacent(last_word, word):
-    //                 if word not in visited:
-    //                     visited.insert(word)
-    //                     new_ladder = copy of ladder
-    //                     new_ladder.push_back(word)
-    //                     if word equals end_word:
-    //                         return new_ladder
-    //                     ladder_queue.push(new_ladder)
-    //     return no ladder found
-    // end function
     std::queue<vector<std::string> > ladder_queue;
     std::vector<std::string> begin_ladder;
     begin_ladder.push_back(begin_word);
@@ -70,7 +51,15 @@ void load_words(set<string> & word_list, const string& file_name) {
 }
 
 void print_word_ladder(const vector<string>& ladder) {
-    return;
+    if (ladder) {
+        std::cout << "Word ladder found: ";
+        for (const auto& word : ladder) {
+            std::cout << word << " ";
+        }        
+    } else {
+        std::cout << "No word ladder found.";
+    }
+    std::cout << '\n';
 }
 
 void verify_word_ladder() {
