7b3a7a7a9c021e55e1f947c87cf64471fc762077
Julia Nguyen <juliatn5@uci.edu>
2025-03-16 06:16:19 +0000

    test

--- src/ladder.cpp
+++ src/ladder.cpp
@@ -4,3 +4,28 @@
 void error(string word1, string word2, string msg) {
     std::cout << word1 << word2 << msg << '\n';
 }
+
+bool edit_distance_within(const std::string& str1, const std::string& str2, int d) {
+    return false;
+}
+
+bool is_adjacent(const string& word1, const string& word2) {
+    return false;
+}
+
+vector<string> generate_word_ladder(const string& begin_word, const string& end_word, const set<string>& word_list) {
+    vector<string> test;
+    return test;
+}
+
+void load_words(set<string> & word_list, const string& file_name) {
+    return;
+}
+
+void print_word_ladder(const vector<string>& ladder) {
+    return;
+}
+
+void verify_word_ladder() {
+    return;
+}
